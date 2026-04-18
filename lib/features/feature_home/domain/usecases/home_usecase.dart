999c94a1e02da3c59c722bf5902498c4921d5246
AmandaFonseca <amandafonseca207@gmail.com>
2026-04-17 21:13:52 -0300

    feat: usecase de informações

---
+++ lib/features/feature_home/domain/usecases/home_usecase.dart
@@ -0,0 +1,17 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_grid_info/core/errors/failures.dart';
+import 'package:flutter_grid_info/features/feature_home/domain/entities/informacao.dart';
+import 'package:flutter_grid_info/features/feature_home/domain/repositories/home_repository.dart';
+
+class HomeUsecase {
+  final HomeRepository repository;
+
+  HomeUsecase({required this.repository});
+
+  Future<Either<Failure, Informacao>> adiciona(Informacao info) async {
+    if (!info.isTextoInfoValido) {
+      return Left(FileFailure());
+    }
+    return await repository.salvarInformacao(info);
+  }
+}
